2c38edb900178f154ab69bbe41ad858741cdbc86
MAGAÑA FABIO HERNAN <20265503757@policia.seguridadciudad.gob.ar>
2026-03-24 22:28:22 -0300

    no important changes

--- src/routes/sheets/sheets.get.routes.js
+++ src/routes/sheets/sheets.get.routes.js
@@ -3,6 +3,6 @@ const { getSheetHandler } = require("../../handlers/sheets/sheets.get.handler");
 const apiKeyMiddleware=require("../../middlewares/apiKey.middleware")
 const router = Router();
 
-router.get("/data/:id", apiKeyMiddleware, getSheetHandler);
+router.get("/data", apiKeyMiddleware, getSheetHandler);
 
 module.exports = router;
